73c1ad9142a95981aa3217878bdcdd03396a6d91
Andrezin <sehilacarvalho@.com>
2025-10-09 11:38:40 -0300

    Seccond commit

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
-import torch
+from llama_cpp import Llama
+import streamlit as st
+import time
 
-model_name = "microsoft/Phi-3.5-mini-instruct"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code = True)
-
-pipe = pipeline("text-generation", model=model_name, tokenizer=tokenizer)
-
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    trust_remote_code=True,
-    torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
+llm = Llama(
+    model_path="/home/andrezib/Desktop/CBT-AI/AI-models/llama-2-7b-chat.Q3_K_M.gguf",
+    n_ctx=2048,
+    max_tokens=1024,
+    temperature=0.7,
     device_map="auto"
 )
 
-messages = [
-    {"role": "system", "content": "You are a helpful AI assistant."},
-    {"role": "user",   "content": "Quem foi Andy Yen?"}
-]
+def generate_response(prompt):
+    output = llm.create_chat_completion(
+    messages = [{"role":"user", "content": prompt}])
+    response = output['choices'][0]['message']["content"]
+    return response
 
-response = pipe(messages, max_new_tokens=512, do_sample=True, top_p=0.7, temperature=0.1)
+def typewritter_effect(text, speed=0.03):
+    container = st.empty()
+    displayed_text = ""
+    for char in text:
+        displayed_text += char
+        container.markdown(f"```\n{displayed_text}█\n```")
+        time.sleep(speed)
+    container.markdown(f"```\n{displayed_text}\n```")
+   
+
+def main():
+    st.title("CBT-AI")
+    st.write("Start your Cognitive Behavioral Therapy Session")
+    user_input = st.text_input("Mensagem:")
+    if st.button("Send"):
+     with st.spinner("In progress..."):
+        time.sleep(2)
+        response = generate_response(user_input)
+        typewritter_effect(response)
+
+if __name__ == "__main__":
+   main()
